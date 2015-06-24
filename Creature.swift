bc52cf58c52749ab44d85ef0dc233099574a1587
Taylor Ferreira <taylorferreira10@gmail.com>
2015-06-24 11:58:31 -0700

    Game Of Life (Make School)

---
+++ Creature.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class Creature: CCSprite {
+    
+    var isAlive = false {
+        didSet {
+            self.visible = isAlive
+    }
+}
+    var livingNeighborsCount = 0
+    
+    convenience override init() {
+            self.init(imageNamed: "GameOfLifeAssets/Assets/bubble.png")
+            anchorPoint = CGPoint(x: 0, y: 0)
+    }
+}   
